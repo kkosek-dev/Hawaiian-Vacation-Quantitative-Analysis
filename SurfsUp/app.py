933789e2da552a0c39f223524ece7acf7a86c315
kkosek-dev <kkosek@alum.utk.edu>
2023-11-12 22:06:17 -0500

    EDA recomplete

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -7,15 +7,17 @@ from sqlalchemy import create_engine
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
+Base.classes.keys()
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
